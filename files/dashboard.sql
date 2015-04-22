464961235038e9b776edab2664f0ee2e5b985bcd
Christian Schwede <christian.schwede@enovance.com>
2015-04-22 21:17:35 +0200

    Reformat the dashboard SQL/JSON to make it more readable
    Also add some disk metrics

--- files/dashboard.sql
+++ files/dashboard.sql
@@ -1,8 +1,169 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "dashboard_dashboard" (
-    "name" varchar(128) NOT NULL PRIMARY KEY,
-    "state" text NOT NULL
-);
-INSERT INTO "dashboard_dashboard" VALUES('default','{"name": "default", "defaultGraphParams": {"width": "400", "from": "-4hours", "until": "now", "height": "300"}, "refreshConfig": {"interval": 60000, "enabled": false}, "graphs": [["target=substr(avg(*.cpu.*.cpu.system)%2C%204)&target=substr(avg(*.cpu.*.cpu.idle)%2C%204)&target=substr(avg(*.cpu.*.cpu.user)%2C%204)", {"title": "Average CPU usage", "from": "-4hours", "until": "now", "target": ["substr(avg(*.cpu.*.cpu.system), 4)", "substr(avg(*.cpu.*.cpu.idle), 4)", "substr(avg(*.cpu.*.cpu.user), 4)"]}, "/render?width=400&from=-4hours&until=now&height=300&target=substr(avg(*.cpu.*.cpu.system)%2C%204)&target=substr(avg(*.cpu.*.cpu.idle)%2C%204)&target=substr(avg(*.cpu.*.cpu.user)%2C%204)&title=Average%20CPU%20usage&_uniq=0.6268012685878336"], ["target=highestCurrent(aliasSub(*.load.load.longterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)&target=highestCurrent(aliasSub(*.load.load.shortterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)&target=highestCurrent(aliasSub(*.load.load.midterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)", {"target": ["highestCurrent(aliasSub(*.load.load.longterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)", "highestCurrent(aliasSub(*.load.load.shortterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)", "highestCurrent(aliasSub(*.load.load.midterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)"], "title": "Highest Load"}, "/render?width=400&from=-4hours&until=now&height=300&target=highestCurrent(aliasSub(*.load.load.longterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)&target=highestCurrent(aliasSub(*.load.load.shortterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)&target=highestCurrent(aliasSub(*.load.load.midterm%2C%22(.*).load.load.(.*)%22%2C%20%22%5C1%20%5C2%22)%2C%205)&title=Highest%20Load&_uniq=0.13428457690796813"], ["target=aliasSub(lowestCurrent(*.df.*.df_complex.free%2C5)%2C%20%22(.*).df.(.*).df_complex.free%22%2C%20%22%5C1%20%5C2%22)", {"target": ["aliasSub(lowestCurrent(*.df.*.df_complex.free,5), \"(.*).df.(.*).df_complex.free\", \"\\1 \\2\")"], "from": "-4hours", "until": "now", "title": "Lowest disk space"}, "/render?width=400&from=-4hours&until=now&height=300&title=Lowest%20disk%20space&target=aliasSub(lowestCurrent(*.df.*.df_complex.free%2C5)%2C%20%22(.*).df.(.*).df_complex.free%22%2C%20%22%5C1%20%5C2%22)&_uniq=0.5118569456065815"], ["target=alias(secondYAxis(avg(stats.timers.*.proxy-server.object.PUT.201.timing.median))%2C%20''PUT%20timing'')&target=alias(secondYAxis(avg(stats.timers.*.proxy-server.object.GET.200.timing.median))%2C%20''GET%20timing'')&target=alias(sum(stats.counters.*.proxy-server.object.PUT.201.xfer.count)%2C%20''PUT'')&target=alias(sum(stats.counters.*.proxy-server.object.GET.200.xfer.count)%2C%20''GET'')", {"target": ["alias(secondYAxis(avg(stats.timers.*.proxy-server.object.PUT.201.timing.median)), ''PUT timing'')", "alias(secondYAxis(avg(stats.timers.*.proxy-server.object.GET.200.timing.median)), ''GET timing'')", "alias(sum(stats.counters.*.proxy-server.object.PUT.201.xfer.count), ''PUT'')", "alias(sum(stats.counters.*.proxy-server.object.GET.200.xfer.count), ''GET'')"], "from": "-4hours", "until": "now", "title": "Proxy traffic/timing"}, "/render?width=400&from=-4hours&until=now&height=300&title=Proxy%20traffic%2Ftiming&target=alias(secondYAxis(avg(stats.timers.*.proxy-server.object.PUT.201.timing.median))%2C%20''PUT%20timing'')&target=alias(secondYAxis(avg(stats.timers.*.proxy-server.object.GET.200.timing.median))%2C%20''GET%20timing'')&target=alias(sum(stats.counters.*.proxy-server.object.PUT.201.xfer.count)%2C%20''PUT'')&target=alias(sum(stats.counters.*.proxy-server.object.GET.200.xfer.count)%2C%20''GET'')&_uniq=0.42796662062110735"], ["target=aliasSub(*.interface.eth*.if_octets.*%2C%22(.*).interface.(.*).if_octets.(.*)%22%2C%20%22%5C1%20%5C2%20%5C3%22)", {"target": ["aliasSub(*.interface.eth*.if_octets.*,\"(.*).interface.(.*).if_octets.(.*)\", \"\\1 \\2 \\3\")"], "title": "Interface usage"}, "/render?width=400&from=-4hours&until=now&height=300&target=aliasSub(*.interface.eth*.if_octets.*%2C%22(.*).interface.(.*).if_octets.(.*)%22%2C%20%22%5C1%20%5C2%20%5C3%22)&title=Interface%20usage&_uniq=0.570059524412934"], ["target=aliasSub(stats.timers.*.*.*.object-server.PUT.timing.median%2C%22stats.timers.*%5C.(.*).object-server.PUT.*%22%2C%20%22PUT%20%5C1%22)&target=aliasSub(stats.timers.*.*.*.object-server.GET.timing.median%2C%22stats.timers.*%5C.(.*).object-server.GET.*%22%2C%20%22GET%20%5C1%22)", {"hideLegend": "false", "target": ["aliasSub(stats.timers.*.*.*.object-server.PUT.timing.median,\"stats.timers.*\\.(.*).object-server.PUT.*\", \"PUT \\1\")", "aliasSub(stats.timers.*.*.*.object-server.GET.timing.median,\"stats.timers.*\\.(.*).object-server.GET.*\", \"GET \\1\")"], "title": "Objectserver timing"}, "/render?width=400&from=-4hours&until=now&height=300&hideLegend=false&target=aliasSub(stats.timers.*.*.*.object-server.PUT.timing.median%2C%22stats.timers.*%5C.(.*).object-server.PUT.*%22%2C%20%22PUT%20%5C1%22)&target=aliasSub(stats.timers.*.*.*.object-server.GET.timing.median%2C%22stats.timers.*%5C.(.*).object-server.GET.*%22%2C%20%22GET%20%5C1%22)&title=Objectserver%20timing&_uniq=0.48020997132089727"], ["target=aliasSub(*.filecount.*quarantined_objects.files%2C%20%22(.*).filecount..*_(.*)_quarantined_objects.files%22%2C%20%22%5C1%20%5C2%22)", {"hideLegend": "false", "target": ["aliasSub(*.filecount.*quarantined_objects.files, \"(.*).filecount..*_(.*)_quarantined_objects.files\", \"\\1 \\2\")"], "title": "Quarantined Objects"}, "/render?width=400&from=-4hours&until=now&height=300&hideLegend=false&target=aliasSub(*.filecount.*quarantined_objects.files%2C%20%22(.*).filecount..*_(.*)_quarantined_objects.files%22%2C%20%22%5C1%20%5C2%22)&title=Quarantined%20Objects&_uniq=0.6139213883911391"], ["target=aliasSub(*.filecount.*quarantined_containers.files%2C%20%22(.*).filecount..*_(.*)_quarantined.*.files%22%2C%20%22%5C1%20%5C2%22)", {"hideLegend": "false", "target": ["aliasSub(*.filecount.*quarantined_containers.files, \"(.*).filecount..*_(.*)_quarantined.*.files\", \"\\1 \\2\")"], "title": "Quarantined Containers"}, "/render?width=400&from=-4hours&until=now&height=300&hideLegend=false&target=aliasSub(*.filecount.*quarantined_containers.files%2C%20%22(.*).filecount..*_(.*)_quarantined.*.files%22%2C%20%22%5C1%20%5C2%22)&title=Quarantined%20Containers&_uniq=0.10001686874422366"], ["target=aliasSub(*.filecount.*quarantined_accounts.files%2C%20%22(.*).filecount..*_(.*)_quarantined.*.files%22%2C%20%22%5C1%20%5C2%22)", {"hideLegend": "false", "target": ["aliasSub(*.filecount.*quarantined_accounts.files, \"(.*).filecount..*_(.*)_quarantined.*.files\", \"\\1 \\2\")"], "title": "Quarantined Accounts"}, "/render?width=400&from=-4hours&until=now&height=300&hideLegend=false&target=aliasSub(*.filecount.*quarantined_accounts.files%2C%20%22(.*).filecount..*_(.*)_quarantined.*.files%22%2C%20%22%5C1%20%5C2%22)&title=Quarantined%20Accounts&_uniq=0.6815994510419167"], ["target=aliasSub(stats.timers.*.*.*.object-auditor.timing.median%2C%20%22stats.timers.*.*%5C.(.*).object-auditor.timing.median%22%2C%20%22%5C1%22)", {"title": "Object Auditor", "from": "-4hours", "until": "now", "target": ["aliasSub(stats.timers.*.*.*.object-auditor.timing.median, \"stats.timers.*.*\\.(.*).object-auditor.timing.median\", \"\\1\")"]}, "/render?width=400&from=-4hours&until=now&height=300&target=aliasSub(stats.timers.*.*.*.object-auditor.timing.median%2C%20%22stats.timers.*.*%5C.(.*).object-auditor.timing.median%22%2C%20%22%5C1%22)&title=Object%20Auditor&_uniq=0.5276944791980962"], ["target=aliasSub(stats.timers.*.*.*.object-replicator.partition.update.timing.median%2C%20%22stats.timers.*.*%5C.(.*).object-replicator.partition.update.timing.median%22%2C%20%22%5C1%22)", {"title": "Object Replicator", "from": "-4hours", "until": "now", "target": ["aliasSub(stats.timers.*.*.*.object-replicator.partition.update.timing.median, \"stats.timers.*.*\\.(.*).object-replicator.partition.update.timing.median\", \"\\1\")"]}, "/render?width=400&from=-4hours&until=now&height=300&target=aliasSub(stats.timers.*.*.*.object-replicator.partition.update.timing.median%2C%20%22stats.timers.*.*%5C.(.*).object-replicator.partition.update.timing.median%22%2C%20%22%5C1%22)&title=Object%20Replicator&_uniq=0.7444726381389938"]], "timeConfig": {"startDate": "2015-04-22T00:00:00", "endTime": "12:00 AM", "endDate": "2015-04-23T00:00:00", "relativeStartQuantity": "4", "relativeUntilQuantity": "", "startTime": "12:00 AM", "relativeStartUnits": "hours", "type": "relative", "relativeUntilUnits": "now"}, "graphSize": {"width": "400", "height": "300"}}');
-COMMIT;
+INSERT INTO "dashboard_dashboard" VALUES('default','
+{
+    "defaultGraphParams": {
+        "from": "-4hours",
+        "height": "300",
+        "until": "now",
+        "width": "400"
+    },
+    "graphSize": {
+        "height": "300",
+        "width": "400"
+    },
+    "graphs": [
+        [
+            "",
+            {
+                "target": [
+                    "substr(avg(*.cpu.*.cpu.system), 4)",
+                    "substr(avg(*.cpu.*.cpu.idle), 4)",
+                    "substr(avg(*.cpu.*.cpu.user), 4)"
+                ],
+                "title": "Average CPU usage"
+            }
+        ],
+        [
+            "",
+            {
+                "target": [
+                    "highestCurrent(aliasSub(*.load.load.longterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)",
+                    "highestCurrent(aliasSub(*.load.load.shortterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)",
+                    "highestCurrent(aliasSub(*.load.load.midterm,\"(.*).load.load.(.*)\", \"\\1 \\2\"), 5)"
+                ],
+                "title": "Highest Load"
+            }
+        ],
+        [
+            "",
+            {
+                "from": "-4hours",
+                "target": [
+                    "aliasSub(lowestCurrent(*.df.*.df_complex.free,5), \"(.*).df.(.*).df_complex.free\", \"\\1 \\2\")"
+                ],
+                "title": "Lowest disk space",
+                "until": "now"
+            }
+        ],
+        [
+            "",
+            {
+                "from": "-4hours",
+                "target": [
+                    "alias(secondYAxis(avg(stats.timers.*.proxy-server.object.PUT.201.timing.median)), ''PUT timing'')",
+                    "alias(secondYAxis(avg(stats.timers.*.proxy-server.object.GET.200.timing.median)), ''GET timing'')",
+                    "alias(sum(stats.counters.*.proxy-server.object.PUT.201.xfer.count), ''PUT'')",
+                    "alias(sum(stats.counters.*.proxy-server.object.GET.200.xfer.count), ''GET'')"
+                ],
+                "title": "Proxy traffic/timing",
+                "until": "now"
+            }
+        ],
+        [
+            "",
+            {
+                "target": [
+                    "aliasSub(*.interface.eth*.if_octets.*,\"(.*).interface.(.*).if_octets.(.*)\", \"\\1 \\2 \\3\")"
+                ],
+                "title": "Interface usage"
+            }
+        ],
+        [
+            "",
+            {
+                "target": [
+                    "aliasSub(*.disk.*.disk_time.*, \"(.*).disk.(.*).disk_time.(.*)\", \"\\1 \\2 \\3\")"
+                ],
+                "title": "Disk time"
+            }
+        ],
+        [
+            "",
+            {
+                "target": [
+                    "aliasSub(*.disk.*.disk_ops.*, \"(.*).disk.(.*).disk_ops.(.*)\", \"\\1 \\2 \\3\")"
+                ],
+                "title": "Disk IOPS"
+            }
+        ],
+        [
+            "",
+            {
+                "hideLegend": "false",
+                "target": [
+                    "aliasSub(stats.timers.*.*.*.object-server.PUT.timing.median,\"stats.timers.*\\.(.*).object-server.PUT.*\", \"PUT \\1\")",
+                    "aliasSub(stats.timers.*.*.*.object-server.GET.timing.median,\"stats.timers.*\\.(.*).object-server.GET.*\", \"GET \\1\")"
+                ],
+                "title": "Objectserver timing"
+            }
+        ],
+        [
+            "",
+            {
+                "hideLegend": "false",
+                "target": [
+                    "aliasSub(*.filecount.*quarantined_objects.files, \"(.*).filecount..*_(.*)_quarantined_objects.files\", \"\\1 \\2\")"
+                ],
+                "title": "Quarantined Objects"
+            }
+        ],
+        [
+            "",
+            {
+                "hideLegend": "false",
+                "target": [
+                    "aliasSub(*.filecount.*quarantined_containers.files, \"(.*).filecount..*_(.*)_quarantined.*.files\", \"\\1 \\2\")"
+                ],
+                "title": "Quarantined Containers"
+            }
+        ],
+        [
+            "",
+            {
+                "hideLegend": "false",
+                "target": [
+                    "aliasSub(*.filecount.*quarantined_accounts.files, \"(.*).filecount..*_(.*)_quarantined.*.files\", \"\\1 \\2\")"
+                ],
+                "title": "Quarantined Accounts"
+            }
+        ],
+        [
+            "",
+            {
+                "from": "-4hours",
+                "target": [
+                    "aliasSub(stats.timers.*.*.*.object-auditor.timing.median, \"stats.timers.*.*\\.(.*).object-auditor.timing.median\", \"\\1\")"
+                ],
+                "title": "Object Auditor",
+                "until": "now"
+            }
+        ],
+        [
+            "",
+            {
+                "from": "-4hours",
+                "target": [
+                    "aliasSub(stats.timers.*.*.*.object-replicator.partition.update.timing.median, \"stats.timers.*.*\\.(.*).object-replicator.partition.update.timing.median\", \"\\1\")"
+                ],
+                "title": "Object Replicator",
+                "until": "now"
+            }
+        ]
+    ],
+    "name": "default",
+    "refreshConfig": {
+        "enabled": false,
+        "interval": 60000
+    },
+    "timeConfig": {
+        "endDate": "2015-04-23T00:00:00",
+        "endTime": "12:00 AM",
+        "relativeStartQuantity": "4",
+        "relativeStartUnits": "hours",
+        "relativeUntilQuantity": "",
+        "relativeUntilUnits": "now",
+        "startDate": "2015-04-22T00:00:00",
+        "startTime": "12:00 AM",
+        "type": "relative"
+    }
+}
+');
